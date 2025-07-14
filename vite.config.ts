f82317500c7e49fd9281823405ef65d59490ac25
moorauder <moorauder@proton.me>
2025-07-13 22:15:02 -0400

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/better-units/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
