b91997c36e4a18772d6bb5a32b8a74da9e7b6d07
moorauder <moorauder@proton.me>
2025-07-13 21:02:37 -0400

    Initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import UnitConverter from '@/components/UnitConverter';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-subtle">
+      <UnitConverter />
+    </div>
+  );
+};
+
+export default Index;
